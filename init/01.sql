eefebaf19eb8256f64b779a98615382325f75dd2
Onexlab <46291202+oxlb@users.noreply.github.com>
2022-05-21 23:17:21 +0800

    Create 01.sql

---
+++ init/01.sql
@@ -0,0 +1,10 @@
+CREATE TABLE student (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR (255),
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO student(id, name) VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C');
